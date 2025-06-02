0ceb640100da3ddb034598f81f2a9618276a5c38
101t <tarek.it.eng@gmail.com>
2025-06-02 06:56:19 +0300

    fixing styles, adding brand, contact, about pages, bug fixes

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path, include
+from django.urls import path
+from django.shortcuts import redirect
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,5 +9,6 @@ urlpatterns = [
     path('about/', views.about_page, name='about'),
     path('contact/', views.contact_page, name='contact'),
     path('product/', views.product_list, name='product_list'),
+    path('products/', lambda request: redirect('product_list', permanent=True)),
     path('product/<slug:slug>/', views.product_details, name='product_details'),
 ]
