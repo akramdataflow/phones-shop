d3f6aec26238932a00ab4e16e9abab2cc2fa7543
101t <tarek.it.eng@gmail.com>
2025-06-04 09:12:50 +0300

    filter removed

--- main/admin/product.py
+++ main/admin/product.py
@@ -15,7 +15,7 @@ class ProductImageInline(admin.TabularInline):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductVariantInline, ProductImageInline]
-    list_display = ('name', 'category', 'price', 'has_size', 'has_color')
-    list_filter = ('category', 'price', 'has_size', 'has_color')
+    list_display = ('name', 'category', 'price', 'has_size', 'has_color', 'is_featured', 'brand', 'label')
+    list_filter = ('category', 'price', 'has_size', 'has_color', 'is_featured', 'brand', 'label')
     search_fields = ('name', 'category__title', 'description',  'price')
     ordering = ('category', 'name')
